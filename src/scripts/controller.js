98fb3573c4e20b1882e11b6737e3bee1b77525ed
David Storey <david@lostincode.co.uk>
2016-02-08 17:51:30 +0000

    Splitting out screen and controller
    And getting of faux local mode

---
+++ src/scripts/controller.js
@@ -0,0 +1,40 @@
+(function() {
+	'use strict';
+
+	var airconsole = new AirConsole({ 'orientation': 'portrait' });
+
+	airconsole.onMessage = function(deviceId, data) {
+		this.dispatchEvent(deviceId, data);
+	};
+
+	var card = new Bingo.Card();
+	var cardView = new Bingo.CardView({
+		el: '#cardPlaceHolder',
+		data: { model: card },
+		oninit: function(options) {
+			this.on('mark', function(e, cell) {
+				airconsole.sendEvent(AirConsole.SCREEN, 'mark', { 'mark': cell });
+			});
+
+			this.on('bingo', function(e, cell) {
+				airconsole.sendEvent(AirConsole.SCREEN, 'bingo', { 'bingo': true });
+			});
+
+			airconsole.on('marked', (function(deviceId, data) {
+				this.get('model').markCellByValue(data.marked.value);
+
+				if (this.get('model').bingoAvailable) {
+					airconsole.sendEvent(AirConsole.SCREEN, 'bingoAvailable', { 'bingoAvailable': true });
+				}
+
+				if (this.get('model').nearlyBingo) {
+					airconsole.sendEvent(AirConsole.SCREEN, 'nearlyBingo', { 'nearlyBingo': true });
+				}
+			}).bind(this));
+		}
+	});
+
+	airconsole.on('reset', function(deviceId, data) {
+		card = new Bingo.Card();
+	});
+})();
---
+++ src/scripts/screen.js
@@ -0,0 +1,91 @@
+/*
+	TODO Start a new game
+	TODO Have seperate screens
+	TODO Replace buttons with divs
+	TODO Better display of bingo result
+	TODO Better display of max players
+	TODO Don't show player card until game begins
+	TODO Better pause option
+*/
+
+
+(function() {
+	'use strict';
+
+	var airconsole = new AirConsole();
+
+	airconsole.onMessage = function(deviceId, data) {
+		this.dispatchEvent(deviceId, data);
+	};
+
+	var callerModel = new Bingo.Caller();
+	var callerView = new Bingo.CallerView({
+		el: '#callerPlaceHolder',
+		data: { model: callerModel },
+		oninit: function() {
+			this.on('start', function(e, cell) {
+				this.get('model').start();
+				airconsole.broadcastEvent('reset', { reset: true });
+			});
+		}
+	});
+
+	window.addEventListener('blur', function() {
+		callerModel.stop();
+	});
+
+	window.addEventListener('focus', function() {
+		callerModel.start();
+	});
+
+	airconsole.on('bingo', function(deviceId, data) {
+		if (!callerModel.bingoCalled) {
+			alert(airconsole.getNickname(deviceId) + ' got bingo!');
+			playersModel.recordWin(deviceId);
+
+			callerModel.stop();
+		}
+	});
+
+	airconsole.on('mark', function(deviceId, data) {
+		if (callerModel.hasBeenCalled(data.mark.value)) {
+			airconsole.sendEvent(deviceId, 'marked', { 'marked': data.mark });
+		}
+	});
+
+	airconsole.on('bingoAvailable', function(deviceId, data) {
+		playersModel.changeState(deviceId, 'bingoAvailable', true);
+	});
+
+	airconsole.on('nearlyBingo', function(deviceId, data) {
+		playersModel.changeState(deviceId, 'nearlyBingo', true);
+	});
+
+	var playersModel = new Bingo.Players();
+	var playersView = new Bingo.PlayersView({
+		el: '#playersPlaceHolder',
+		data: { model: playersModel },
+		oninit: function() {
+
+		}
+	});
+
+	// var screensModel = new Bingo.Screens();
+	// var screensView = new Bingo.ScreensView({
+	// 	el: '#screensPlaceHolder',
+	// 	data: { model: screensModel },
+	// 	oninit: function() {
+	// 		this.on('onScreen', function(e, screen) {
+	// 			return (this.get('model').current === screen);
+	// 		});
+	// 	}
+	// });
+
+	airconsole.onConnect = function(deviceId) {
+		playersModel.add(deviceId, airconsole.getNickname(deviceId), airconsole.getProfilePicture(deviceId));
+	};
+
+	airconsole.onDisconnect = function(deviceId) {
+		playersModel.remove(deviceId);
+	};
+})();
