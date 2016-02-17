d06cac73977b2f7f569af3a95d66b162cd4f0c32
David Storey <david@lostincode.co.uk>
2016-02-17 08:55:20 +0000

    Adding range of colours and background noise

--- src/scripts/models/players.js
+++ src/scripts/models/players.js
@@ -1,7 +1,7 @@
 (function() {
 	'use strict';
 
-	var colourPool = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+	var colourPool = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21];
 
 	var PlayerModel = require('./player');
 
